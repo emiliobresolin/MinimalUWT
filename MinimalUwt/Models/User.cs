5a0f9c3eba70c22feb5fcc372cfc79ba708db129
emiliobresolin <emiliotdm@hotmail.com>
2023-03-25 11:03:24 -0300

    adding User class to Models

---
+++ MinimalUwt/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace MinimalUwt.Models
+{
+    public class User
+    {
+        //public int Id { get; set; }
+        public string Username { get; set; }
+        public string EmailAdress { get; set; }
+        public string Password { get; set; }
+        public string GivenName { get; set; }
+        public string Surname { get; set;}
+        public string Role { get; set; }
+            
+    }
+}
