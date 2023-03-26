c86c0647324a47541470e9b9f57c8eed62043729
emiliobresolin <emiliotdm@hotmail.com>
2023-03-26 09:26:46 -0300

    adding movie interface for movie service in services folder

---
+++ MinimalUwt/Services/IMovieService.cs
@@ -0,0 +1,16 @@
+﻿using MinimalUwt.Models;
+
+namespace MinimalUwt.Services
+{
+    public interface IMovieService
+    {
+        public interface IMovieService
+        {
+            public Movie Create(Movie movie);
+            public Movie Get(int id);
+            public List<Movie> List();
+            public Movie Update(Movie movie);
+            public bool Delete(int id);
+        }
+    }
+}
