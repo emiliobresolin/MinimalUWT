92f5d3e57f56b7f9cdb35cc7ebe85e42d291e791
emiliobresolin <emiliotdm@hotmail.com>
2023-03-26 09:48:01 -0300

    adding references for endpoints in program.cs

--- MinimalUwt/Program.cs
+++ MinimalUwt/Program.cs
@@ -1,4 +1,11 @@
+using MinimalUwt.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSingleton<IMovieService, MovieService>();
+builder.Services.AddSingleton<IUserService, UserService>();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
